575065855c69fb9370e782ee6da7f37f68a60eb6
velandia98 <vanessa.velandia98@gmail.com>
2021-09-18 10:30:55 -0500

    primer trabajo

---
+++ main.py
@@ -0,0 +1,10 @@
+from persona import Persona
+
+estudiante = Persona("Ana")
+estudiante = Persona("Isabel")
+estudiante = Persona ("Alejandro")
+estudiante.autenticar()
+estudiante.edad
+texto = "ejemeplo"
+texto.replace("a", "e")
+print
